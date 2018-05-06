4867f2c1c79a0df0815e28fa9c1372aa4dc11d66
higomiku <higomiku2012@gmail.com>
2018-05-06 20:10:15 +0900

    ハッシュタグで情報を取得できるように修正

--- src/main/java/com.higomiku/Main.java
+++ src/main/java/com.higomiku/Main.java
@@ -6,7 +6,10 @@ public class Main{
     public static void main(String[]args){
         System.out.println("HelloWorld");
 
-        TwitterScraper.testScrapeFromTwitter();
+        //引数のハッシュタグ名を取得
+        //String hash_tag_name = args[0];
+        String hash_tag_name = "サマーランド";
+        TwitterScraper.getTweetByHashTagName(hash_tag_name);
 
     }
 }
--- src/main/java/com.higomiku/TwitterScraper.java
+++ src/main/java/com.higomiku/TwitterScraper.java
@@ -1,26 +1,42 @@
 package com.higomiku;
 
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.User;
+import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
+
+import java.util.List;
 
 public class TwitterScraper {
 
-    protected static void testScrapeFromTwitter(){
-        // 初期化
-        Twitter twitter = new TwitterFactory().getInstance();
+    //ハッシュタグでtweetを取得するメソッド
+    protected static void getTweetByHashTagName(String hashTagName){
+        Twitter twitter = createTwitterInstance();
+        Query query = new Query();
+        query.setQuery("#" + hashTagName);
+
+        QueryResult result = null;
+
+        try {
+            result = twitter.search(query);
+            List<Status> tweetList = result.getTweets();
+            System.out.println("tweetList = " + tweetList.size());
+
+            for(Status tweet : tweetList){
+                System.out.println("結果");
+                System.out.println(tweet.getText());
+            }
+        } catch (TwitterException e) {
+            e.printStackTrace();
+            System.out.println("ERROR searchでエラーが発生しました");
+        }
 
 
-//        ConfigurationBuilder cb = new ConfigurationBuilder();
-//        cb.setDebugEnabled(true)
-//                .setOAuthConsumerKey("cp4oMmjnSQd8P3RuTsQ5EAaqs")
-//                .setOAuthConsumerSecret("eie0tHGrjW7MiQr5fjGbLtJcaKKVF551ydKmQ9Bg8B0K0Uudit")
-//                .setOAuthAccessToken("264043200-1jTOvNAAqdbQFzumB9wJAuRZXAxrSEDdOQ6YUMEx")
-//                .setOAuthAccessTokenSecret("6KnWGTXfPRsDkZs6A1cZBEWjOGbQbTZDQ54kXITTcIIEL");
-//        TwitterFactory tf = new TwitterFactory(cb.build());
-//        Twitter twitter = tf.getInstance();
+
+    }
+
+
+    //ユーザ情報取得メソッド
+    protected static void getUserInfo() {
+        Twitter twitter = createTwitterInstance();
         User user = null;
 
         {
@@ -36,5 +52,25 @@ public class TwitterScraper {
         System.out.println("ひょうじ名　：" + user.getScreenName());
         System.err.println("ふぉろー数　：" + user.getFriendsCount());
         System.out.println("ふぉろわー数：" + user.getFollowersCount());
+    }
+
+
+    //Twitterインスタンス生成
+    protected static Twitter  createTwitterInstance(){
+        // 初期化
+
+//        Twitter twitter = new TwitterFactory().getInstance();
+
+
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("cp4oMmjnSQd8P3RuTsQ5EAaqs")
+                .setOAuthConsumerSecret("eie0tHGrjW7MiQr5fjGbLtJcaKKVF551ydKmQ9Bg8B0K0Uudit")
+                .setOAuthAccessToken("264043200-1jTOvNAAqdbQFzumB9wJAuRZXAxrSEDdOQ6YUMEx")
+                .setOAuthAccessTokenSecret("6KnWGTXfPRsDkZs6A1cZBEWjOGbQbTZDQ54kXITTcIIEL");
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
+        return twitter;
+
     }
 }
